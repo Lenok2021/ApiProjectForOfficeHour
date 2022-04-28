0ad4b595fdc98d497715a2b07a83f926dce3c4b4
cross <crosstown11223@yahoo.com>
2022-04-28 10:20:50 -0400

    init commit

--- src/test/java/com/cydeo/day1/Spartans.java
+++ src/test/java/com/cydeo/day1/Spartans.java
@@ -1,26 +1,29 @@
 package com.cydeo.day1;
 
-import com.cydeo.utilities.SpartansTestBase;
+
 import io.restassured.RestAssured;
+
+import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class Spartans {
 
     @BeforeAll
-    public void createBeforeAll() {
+    public static void createBeforeAll() {
         RestAssured.baseURI = "http://54.234.53.165:8000";
         RestAssured.basePath = "/api";
     }
-        @AfterAll
-        public void reset() {
+
+    @AfterAll
+    public  static void reset() {
 
         RestAssured.reset();
 
-        }
-
-
+    }
 
 
     @Test
@@ -41,8 +44,39 @@ public class Spartans {
      - Get me all spartan name
 
       */
+        //Store the response in Response Object that comes from get Request
+        Response response = RestAssured.get("/spartans");
 
+        //print response
+        System.out.println(response.prettyPrint());
 
+        //print Content-Type
+        System.out.println(response.contentType());
+        //print status code
+        System.out.println(response.statusCode());
+
+        //Get me third spartan
+
+        Object thirdSpartan = response.path("[2]");
+        System.out.println("thirdSpartan = " + thirdSpartan);
+
+        //Get me third spartan gender
+
+        String thirdSpartanGender = response.path("[2].gender");
+        System.out.println("thirdSpartanGender = " + thirdSpartanGender);
+
+        
+        //Get me third spartan name
+        String thirdSpartanName = response.path("[2].name");
+        System.out.println("thirdSpartanName = " + thirdSpartanName);
+        
+        // Get me all spartan name
+        
+        List<String> names = response.path("name");
+        System.out.println("names = " + names);
+
+        
+        
     }
 
 
